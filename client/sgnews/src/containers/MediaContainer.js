693edd9d02caedc0eae4e8079aa7fe093374ac4f
EnigmaticGale <hamishwhytehw@gmail.com>
2019-05-31 15:39:36 +0100

    set up api and made fix in java project

--- client/sgnews/src/containers/MediaContainer.js
+++ client/sgnews/src/containers/MediaContainer.js
@@ -3,8 +3,33 @@ import MediaTitle from '../components/MediaTitle';
 import MediaList from '../components/MediaList';
 
 class MediaContainer extends Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      articles: [],
+      journalists: []
+    }
+  }
+  
+  componentDidMount() {
+    const articleUrl = 'http://localhost:8080/articles/bydate';
+    const journalistsUrl = 'http://localhost:8080/journalists'
+    fetch(articleUrl)
+    .then(res => res.json())
+    .then((articles) => {
+      this.setState({ articles: articles });
+    });
+    fetch(journalistsUrl)
+    .then(res => res.json())
+    .then((journalistsData) => {
+      const journalists = journalistsData["_embedded"].journalists;
+      this.setState({ journalists: journalists });
+    });
+  }
 
     render() {
+
+
 
       return (
         <div className="media-container">
