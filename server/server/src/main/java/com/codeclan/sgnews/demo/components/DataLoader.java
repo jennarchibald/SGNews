ca8a9a34ff279815a8a20f9938e7eafbfdf425a8
Barker <benbarks.v1@gmail.com>
2019-06-03 10:32:27 +0100

    bios added to journalist

--- server/server/src/main/java/com/codeclan/sgnews/demo/components/DataLoader.java
+++ server/server/src/main/java/com/codeclan/sgnews/demo/components/DataLoader.java
@@ -24,16 +24,16 @@ public class DataLoader implements ApplicationRunner {
     }
 
     public void run(ApplicationArguments args){
-        Journalist ben = new Journalist("Ben", "no image found");
+        Journalist ben = new Journalist("Ben", "no image found", "In Soviet Russia, news report you");
         journalistRepository.save(ben);
 
-        Journalist grant = new Journalist("Grant", "no image found");
+        Journalist grant = new Journalist("Grant", "no image found", "I’m indifferent to most items on the planet. Some people call me a Social Media Guru. I don’t correct them.");
         journalistRepository.save(grant);
 
-        Journalist hamish = new Journalist("Hamish", "no image found");
+        Journalist hamish = new Journalist("Hamish", "no image found", "90% of your problems can be solved by marketing.  Solving the other 10% just requires good procrastination skills.");
         journalistRepository.save(hamish);
 
-        Journalist jenn = new Journalist("Jenn", "no image found");
+        Journalist jenn = new Journalist("Jenn", "no image found", "Insert pretentious crap about myself here.");
         journalistRepository.save(jenn);
 
         Article article = new Article("It's in the Database!", "We've saved it!", "It has successfully saved", "no picture", ben, "2019-05-31T11:13:12.725Z", Category.TECH, Region.HIGHLANDS);
