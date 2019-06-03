b7bc99309a81154b9b83f3ed8c346b6a5549b56f
Grant Rutherford <contact.grantr@gmail.com>
2019-06-03 17:55:20 +0100

    New Article Form updated with binds + onChange

--- client/sgnews/src/components/NewArticleForm.js
+++ client/sgnews/src/components/NewArticleForm.js
@@ -5,7 +5,7 @@ class NewArticleForm extends Component {
     super(props);
 
     this.state = {
-      catagory: "",
+      category: "",
       headline: "",
       image: "",
       journalist: "",
@@ -13,49 +13,95 @@ class NewArticleForm extends Component {
       storytext: "",
       summary: ""
     }
+
+    this.handleCategoryChange = this.handleCategoryChange.bind(this);
+    this.handleHeadlineChange = this.handleHeadlineChange.bind(this);
+    this.handleImageChange = this.handleJournalistChange.bind(this);
+    this.handleJournalistChange = this.handleJournalistChange.bind(this);
+    this.handleRegionChange = this.handleRegionChange.bind(this);
+    this.handleStoryTextChange = this.handleStoryTextChange.bind(this);
+    this.handleSummaryChange = this.handleSummaryChange.bind(this);
   }
+
+
+  handleCategoryChange(event){
+    this.setState({category: event.target.value});
+  };
+
+  handleHeadlineChange(event){
+    this.setState({headline: event.target.value});
+  };
+
+  handleImageChange(event){
+    this.setState({image: event.target.value});
+  };
+
+  handleJournalistChange(event){
+    this.setState({journalist: event.target.value});
+  };
+
+  handleRegionChange(event){
+    this.setState({region: event.target.value});
+  };
+
+  handleStoryTextChange(event){
+    this.setState({storytext: event.target.value});
+  };
+
+  handleStorySummary(event){
+    this.setState({summary: event.target.value});
+  };
 
     render() {
       return (
         <form>
           <div className="form_wrap" >
             <label htmlFor="headline">Headline:</label>
-            <input type="text" name="headline"/>
+            <input 
+            type = "text" 
+            name = "headline"
+            onChange = {this.handleHeadlineChange}/>
           </div>
 
           <div className="form_wrap" >
             <label htmlFor="summary">Summary:</label>
-            <input type="text" name="summary"/>
-
-
+            <input 
+            type="text" 
+            name="summary"
+            onChange = {this.handleSummaryChange}/>
           </div>
 
           <div className="form_wrap" >
             <label htmlFor="storyText">Story Text:</label>
-            <textarea type="text" rows="5" cols="40" name="storyText"/>
-
-
+            <textarea 
+            type="text" 
+            rows="5" cols="40" 
+            name="storyText"
+            onChange = {this.handleStoryTextChange}/>
           </div>
 
           <div className="form_wrap" >
             <label htmlFor="region">Region:</label>
-            <input type="text" name="region"/>
-
-
+            <input 
+            type="text" 
+            name="region"
+            onChange = {this.handleRegionChange}/>
           </div>
 
           <div className="form_wrap" >
-            <label htmlFor="catagory">catagory:</label>
-            <input type="text" name="catagory"/>
-
-
+            <label htmlFor="category">Category:</label>
+            <input 
+            type="text" 
+            name="catagory"
+            onChange = {this.handleCategoryChange}/>
           </div>
 
           <div className="form_wrap" >
-            <label htmlFor="image">image here:</label>
-            <input type="text" name="image"/>
-
-
+            <label htmlFor="image">Image Here:</label>
+            <input 
+            type="text" 
+            name="image"
+            onChange = {this.handleImageChange}/>
           </div>
 
           <div className="form_wrap" >
@@ -64,7 +110,9 @@ class NewArticleForm extends Component {
               {this.props.journalists.map((journalist)=>
                 <option
                 key={journalist.id}
-                value={`http://localhost:8080/journalist/${journalist.id}`}> {journalist.name}
+                value={`http://localhost:8080/journalist/${journalist.id}`}
+                onChange = {this.handleJournalistChange}> 
+                {journalist.name}
                 </option>)}
             </select>
 
