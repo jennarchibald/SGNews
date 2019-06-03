f0cbf9b1f9c47aafb2c92a90808eb65e87675d4f
Grant Rutherford <contact.grantr@gmail.com>
2019-06-03 14:20:02 +0100

    Error page added

--- client/sgnews/src/containers/MediaContainer.js
+++ client/sgnews/src/containers/MediaContainer.js
@@ -5,6 +5,7 @@ import ArticleList from '../components/ArticleList';
 import JournalistList from '../components/JournalistList';
 import FullArticleInfo from '../components/FullArticleInfo';
 import FullJournalistInfo from '../components/FullJournalistInfo';
+import ErrorPage from '../components/ErrorPage';
 import NavBar from '../components/NavBar';
 
 class MediaContainer extends Component{
@@ -70,6 +71,9 @@ class MediaContainer extends Component{
             return (
             <FullJournalistInfo journalist = {journalist} />
           )}}}
+          />
+          <Route
+          component = {ErrorPage}
           />
           </Switch>
         </Router>
