377ba2b29c614cc9f0369a40ac02ede6f53ded5c
EnigmaticGale <hamishwhytehw@gmail.com>
2019-06-03 15:43:02 +0100

    changed state and started filling in form details

--- client/sgnews/src/components/NewArticleForm.js
+++ client/sgnews/src/components/NewArticleForm.js
@@ -6,15 +6,60 @@ class NewArticleForm extends Component {
 
     this.state = {
       catagory: "",
-      date: "",
       headline: "",
-      id: "",
       image: "",
       journalist: "",
       region: "",
       storytext: "",
       summary: ""
     }
+
+    render (
+      return (
+        <form>
+          <div className="form_wrap" >
+            <label htmlFor="headline">Headline:</label>
+
+          </div>
+
+          <div className="form_wrap" >
+            <label htmlFor="summary">Summary:</label>
+
+
+          </div>
+
+          <div className="form_wrap" >
+            <label htmlFor="storyText">Story Text:</label>
+
+
+          </div>
+
+          <div className="form_wrap" >
+            <label htmlFor="region">Region:</label>
+
+
+          </div>
+
+          <div className="form_wrap" >
+            <label htmlFor="catagory">catagory:</label>
+
+
+          </div>
+
+          <div className="form_wrap" >
+            <label htmlFor="image">image here:</label>
+
+
+          </div>
+
+          <div className="form_wrap" >
+            <label htmlFor="journalist">journalist:</label>
+
+
+          </div>
+
+      )
+    )
   }
 }
 
