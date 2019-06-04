9e415ff51d8cace1864cfe4dcbc4159242fdd9cd
Grant Rutherford <contact.grantr@gmail.com>
2019-06-04 17:22:36 +0100

    delete popover complete added

--- client/sgnews/src/components/EditorFullArticleInfo.js
+++ client/sgnews/src/components/EditorFullArticleInfo.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
+import DeletePopover from './DeletePopover';
 
 class EditorFullArticleInfo extends Component {
   render() {
@@ -7,11 +8,16 @@ class EditorFullArticleInfo extends Component {
       <div>
         <h1>{this.props.article.headline}</h1>
         <h4>{this.props.article.date.substring(0, 10)}</h4>
+
         <Link to = {`/editor/articles/${this.props.article.id}/edit`}>EDIT</Link>
+
         <img src={this.props.article.image} alt={this.props.article.summary}></img>
         <h3>{this.props.article.summary}</h3>
         <p>{this.props.article.storyText}</p>
         <h4>Article written by {this.props.article.journalist.name}</h4>
+
+        <DeletePopover></DeletePopover>
+
       </div>
       );
     }
