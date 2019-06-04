6c305b56c8be67852468a6571f429fa2a3e5e87e
Grant Rutherford <contact.grantr@gmail.com>
2019-06-04 16:50:31 +0100

    Delete Popover added to journalist

--- client/sgnews/src/components/EditorFullJournalistInfo.js
+++ client/sgnews/src/components/EditorFullJournalistInfo.js
@@ -1,7 +1,9 @@
 import React, { Component, Fragment } from 'react';
 import {Link} from 'react-router-dom';
+import DeletePopover from './DeletePopover';
 
 class EditorFullJournalistInfo extends Component{
+  
 
   render(){
     let articles = [];
@@ -19,7 +21,11 @@ class EditorFullJournalistInfo extends Component{
         <h1>{this.props.journalist.name}</h1>
         <img src={this.props.journalist.image} alt = ""></img>
         <p>{this.props.journalist.bio}</p>
+
         <Link to ={`/editor/journalists/${this.props.journalist.id}/edit`}>EDIT</Link>
+
+        <DeletePopover></DeletePopover>
+        
         {articles}
         </div>
       )
