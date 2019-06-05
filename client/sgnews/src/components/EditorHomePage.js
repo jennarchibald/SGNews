1ea55b9ab43c5c27ae3759fe3a82231492c8667e
jennarchibald <jennarchibald91@gmail.com>
2019-06-05 16:46:06 +0100

    editor page styling

--- client/sgnews/src/components/EditorHomePage.js
+++ client/sgnews/src/components/EditorHomePage.js
@@ -2,37 +2,49 @@ import React from 'react';
 import MediaTitle from './MediaTitle';
 import Button from '@material-ui/core/Button';
 import {Link} from 'react-router-dom';
+import './EditorHomePage.css'
 
 const EditorHomePage = (props) => {
   if (props.editor){
     return (
       <>
-      <MediaTitle title = "Hello, Editor!"/>
-
       <div>
+      <MediaTitle title = "Hello, Editor!"/>
+      </div>
+
+      <div className = "editor-welcome">
+      <p> What would you like to do today?</p>
+      </div>
+
+      <div className = "editor-buttons">
+      <div className = "add-new">
+      <Link to = "/editor/articles/new">
+      <Button
+      variant="contained"
+      >
+      ADD NEW ARTICLE
+      </Button>
+      </Link>
+      </div>
+      <div className = "add-new">
+      <Link
+      to = "/editor/journalists/new"
+      >
+      <Button
+      variant="contained"
+      className = "add-new">
+      ADD NEW JOURNALIST
+      </Button>
+      </Link>
+      </div>
+      </div>
+
+      <div className = "editor-login">
       <Button
       variant="contained"
       onClick = {props.logOut}>
       LOG OUT
       </Button>
-      </div>
-
-      <div>
-      <Link to = "/editor/articles/new">
-      <Button
-      variant="contained">
-      ADD NEW ARTICLE
-      </Button>
-      </Link>
-      </div>
-
-      <div>
-      <Link to = "/editor/journalists/new">
-      <Button
-      variant="contained">
-      ADD NEW JOURNALIST
-      </Button>
-      </Link>
       </div>
 
       </>
@@ -42,11 +54,13 @@ const EditorHomePage = (props) => {
 
       <>
       <MediaTitle title = "Please Log In"/>
+      <div className = "editor-login">
       <Button
       variant="contained"
       onClick = {props.logIn}>
       LOG IN
       </Button>
+      </div>
       </>
     )
   }
--- client/sgnews/src/components/MediaTitle.js
+++ client/sgnews/src/components/MediaTitle.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import './MediaTitle.css'
 
 const MediaTitle = (props) => {
-
 
     return (<h1>{props.title}</h1>
 
