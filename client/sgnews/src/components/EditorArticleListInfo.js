4c9caf45ba9d55a7f37c3a6e415a5cf6748f518b
jennarchibald <jennarchibald91@gmail.com>
2019-06-05 15:48:55 +0100

    removing edit links from index page

--- client/sgnews/src/components/EditorArticleListInfo.js
+++ client/sgnews/src/components/EditorArticleListInfo.js
@@ -8,7 +8,7 @@ class EditorArticleListInfo extends Component {
     return (
       <div className="editor-article-info">
 
-      <li><Link to = {`/editor/articles/${this.props.children.id}`}>{this.props.children.headline} </Link>| {this.props.children.summary} <Link to = {`/editor/articles/${this.props.children.id}/edit`}>EDIT</Link></li>
+      <li><Link to = {`/editor/articles/${this.props.children.id}`}>{this.props.children.headline} </Link>| {this.props.children.summary} </li>
       </div>
       );
     }
